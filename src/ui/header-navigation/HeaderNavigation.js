8f9094aae7edb71fc03e7f81c26bddbb61aa7723
Yves Schleich <yves.schleich@aboutyou.de>
2017-01-03 09:19:36 +0100

    Added base structure for header-navigation

--- src/ui/header-navigation/HeaderNavigation.js
+++ src/ui/header-navigation/HeaderNavigation.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 export const HeaderNavigation = () => {
-    const headerNavigation = {
-        'padding': '10px 0'
-    };
+    const styles = {
+        headerNavigation: {
+            padding: '20px 0',
 
-    const mainMenuLi = {
-        'display': 'inline-block',
-        'padding': '0 5px'
+            fontSize: 12,
+            fontWeight: 600,
+            letterSpacing: 1,
+            textTransform: 'uppercase'
+        },
+        mainMenuLi: {
+            display: 'inline-block',
+            padding: '0 5px'
+        }
     };
 
     return(
-        <div className="header-navigation" style={headerNavigation}>
+        <div className="header-navigation" style={styles.headerNavigation}>
             <div className="container">
                 <div className="row">
                     <div className="col-xs-2 col-sm-4">
@@ -19,9 +25,9 @@ export const HeaderNavigation = () => {
                     </div>
                     <div className="main-menu col-xs-2 col-sm-8 end-xs">
                         <ul>
-                            <li style={mainMenuLi}>Home</li>
-                            <li style={mainMenuLi}>Kompetenzen</li>
-                            <li style={mainMenuLi}>Kontakt</li>
+                            <li style={styles.mainMenuLi}><a href="/">Home</a></li>
+                            <li style={styles.mainMenuLi}><a href="/">Kompetenzen</a></li>
+                            <li style={styles.mainMenuLi}><a href="/">Kontakt</a></li>
                         </ul>
                     </div>
                 </div>
